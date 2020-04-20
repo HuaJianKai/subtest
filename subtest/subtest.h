b895847529115172f4ca1af1f7ef7ed3fb8c089b
HuaJianKai <41730778+HuaJianKai@users.noreply.github.com>
2020-04-20 14:24:49 +0800

    初始化

---
+++ subtest/subtest.h
@@ -0,0 +1,15 @@
+//
+//  subtest.h
+//  subtest
+//
+//  Created by j k h on 2020/4/20.
+//  Copyright © 2020 j k h. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface subtest : NSObject
+
++ (void)printA;
+
+@end
---
+++ subtest/subtest.m
@@ -0,0 +1,18 @@
+//
+//  subtest.m
+//  subtest
+//
+//  Created by j k h on 2020/4/20.
+//  Copyright © 2020 j k h. All rights reserved.
+//
+
+#import "subtest.h"
+
+@implementation subtest
+
++ (void)printA
+{
+    NSLog(@"123");
+}
+
+@end
